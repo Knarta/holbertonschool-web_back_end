d44864dfed2272c5a09c2ac7403021d77b5ba333
knarta <charquen10@gmail.com>
2025-03-27 10:13:57 +0100

    Resolve undefined variable in toString() method

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+export default class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;  
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set name(name) {
+        return this._name = name;
+    }
+
+    set code(code) {
+        return this._code = code;
+    }
+
+    get [Symbol.toStringTag]() {
+        return 'Airport';
+    }
+
+    toString() {
+        return `[object ${this_code}]`;
+    }
+}
