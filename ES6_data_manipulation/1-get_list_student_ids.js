c9a70f26bb638bbb4cdd06f0eceb964eecda7c96
knarta <charquen10@gmail.com>
2025-03-31 20:36:45 +0200

    feat: add getListStudentIds function to retrieve IDs from student objects array

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds(arr) {
+    if (Array.isArray(arr) === false || arr.every((obj) => typeof obj !== 'object')) {
+        return [];
+    }
+    return arr.map((obj) => obj.id);
+}
