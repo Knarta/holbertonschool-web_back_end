bf97ea104fdef7719267f3f37b75f4a0ac512b23
knarta <charquen10@gmail.com>
2025-04-01 00:03:19 +0200

    updateStudentGradeByCity

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,8 +3,8 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
    .filter((students) => students.location === city)
    .map((student) => {
-     const newGrades = newGrades.find((grade) => grade.studentId === student.id);
-     const grade = !newGrades ? 'N/A' : newGrades.grade;
+     const newGrade = newGrades.find((grade) => grade.studentId === student.id);
+     const grade = !newGrade ? 'N/A' : newGrade.grade;
      return { ...student, grade };
    });
 }
