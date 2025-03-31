41865c588523e03459f3d5294f7e764344dc2988
knarta <charquen10@gmail.com>
2025-04-01 00:05:52 +0200

    update createInt8TypedArray function to improve readability and ensure consistent syntax

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
-    const buffer = new ArrayBuffer(length);
-    const Int8View = new Int8Array(buffer);
-    Int8View[position] = value;
-    return new DataView(buffer);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const Int8View = new Int8Array(buffer);
+  Int8View[position] = value;
+  return new DataView(buffer);
 }
