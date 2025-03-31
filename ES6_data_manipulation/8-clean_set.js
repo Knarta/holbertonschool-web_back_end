f2640dd456b8442f31843050f67f7d2ae033292e
knarta <charquen10@gmail.com>
2025-03-31 23:39:17 +0200

    add cleanSet function to filter and format set values based on startString

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+    if (typeof startString !== 'string' || startString.length === 0) {
+      return '';    
+    }
+    const strings = [...set]
+        .filter((value) => value && value.startsWith(startString))
+        .map((value) => value.slice(startString.length));
+        
+    return strings.join('-');
+}
