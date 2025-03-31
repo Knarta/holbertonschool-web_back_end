0e5664da4a871da87ca2a1e3b63757be867f8f89
knarta <charquen10@gmail.com>
2025-04-01 00:01:30 +0200

    update updateStudentGradeByCity tIds function to use arrow function syntax

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-     .filter((students) => students.location === city)
-     .map((student) => {
-        const newGrades = newGrades.find((grade) => grade.studentId === student.id);
-        const grade = !newGrades ? 'N/A' : newGrades.grade;
-        return { ...student, grade };
-     });
+  return students
+   .filter((students) => students.location === city)
+   .map((student) => {
+     const newGrades = newGrades.find((grade) => grade.studentId === student.id);
+     const grade = !newGrades ? 'N/A' : newGrades.grade;
+     return { ...student, grade };
+   });
 }
