81bd6987c574388cd21cd5d7a50e89298b781852
knarta <charquen10@gmail.com>
2025-04-01 19:13:25 +0200

    git commit -m "Fix handleProfileSignup function: correct variable naming, template literal usage, and property access

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils.js'
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhoto, user]) => {
-      console.log('${photo.body} ${user.first_name} ${user.last_name}');
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline')
