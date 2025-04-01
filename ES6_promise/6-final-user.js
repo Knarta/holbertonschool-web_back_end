46332edf6275fa3a339b306b2be0a99a669e95ab
knarta <charquen10@gmail.com>
2025-04-01 19:34:31 +0200

    Fix handleProfileSignup: correct function name and error handling in Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@
 import { signUpUser } from './4-user-promise.js'
 import { uploadPhoto } from './5-photo-reject.js'
 
-export default function andleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName),])
-    .then((results) => results.map(({status, value, reason }) => ({ status, value: value || reason,})));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map(({status, value, reason }) => ({ status, value: status === 'fulfilled' ? value : reason.message })));
 }
