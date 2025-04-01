da264710d100464f2e224cb3fb867b841012cb8d
knarta <charquen10@gmail.com>
2025-04-01 19:49:07 +0200

    convert to arrow function and maintain error handling

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default function divideFunction(numerator, denominator) {
+export default (numerator, denominator) => {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   }
