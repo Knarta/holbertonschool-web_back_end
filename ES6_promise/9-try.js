70e0a9273d3c98c5f9bbbb460fd28ffa89be1cda
knarta <charquen10@gmail.com>
2025-04-01 19:06:07 +0200

    Implement guardrail function with error handling

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const results = mathFunction();
+    queue.push(results);
+  } catch(error) {
+    queue.push(error.msg);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
