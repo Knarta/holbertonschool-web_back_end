49dec657fb5cbfbd886720cc028ed65e989bbe63
knarta <charquen10@gmail.com>
2025-04-01 16:52:02 +0200

    add handleResponseFromAPI function to process Promise results

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+  const msg : 'Got a response from the API';
+  .then(() => {
+    console.log(msg);
+    return {
+      status: 200,
+      body: 'success',
+    };
+  })
+  .catch(() => {
+    console.log(msg);
+    return new Error();
+  });
+}
