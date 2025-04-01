770e6ce4ddf457345d32bc986a4149fae5afcb91
knarta <charquen10@gmail.com>
2025-04-01 19:43:09 +0200

    "Refactor handleProfileSignup: Use async function and improve error formatting in Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
-import { signUpUser } from './4-user-promise.js'
-import { uploadPhoto } from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map(({ status, value, reason }) => ({
-      status,
-      value: status === "fulfilled" ? value : reason.message,
+    .then((result) => result.map((promise) => ({
+      status: promise.status,
+      value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+
     })));
 }
