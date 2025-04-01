cd65cb9063ffcf687e5fdeb160caea3433684633
knarta <charquen10@gmail.com>
2025-04-01 20:13:02 +0200

    "Fix guardrail function: use error.message instead of error.msg for proper error handling

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -3,10 +3,10 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const results = mathFunction();
+    const results = (mathFunction());
     queue.push(results);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString());
   }
   queue.push('Guardrail was processed');
   return queue;
