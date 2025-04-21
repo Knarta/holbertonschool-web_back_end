27989ecc7325ca5abb221812fb80b96d7375f053
knarta <charquen10@gmail.com>
2025-04-21 13:10:46 +0200

    Add new module 3-to_str: Implement function to convert a number to a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str - Convert a number to a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a number to a string and return the result
+
+    Args:
+        n: float
+    Returns:
+        str: The string representation of n
+    """
+    return str(n)
