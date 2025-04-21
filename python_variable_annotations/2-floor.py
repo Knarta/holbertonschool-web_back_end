18811483d17abdb35cf6464a260430348d22c770
knarta <charquen10@gmail.com>
2025-04-21 13:07:48 +0200

    Refactor docstring in 2-floor module: Clean up formatting and improve readability

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,13 +3,14 @@
 Module 2-floor - Floor a floating number
 """
 
+
 def floor(n: float) -> int:
     """
     Floor a floating number and return the result
-    
+
     Args:
     n: float
-    
+
     Returns:
         int: The floor of n
     """
