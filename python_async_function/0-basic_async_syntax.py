0e67d3978760545abec09466c65d8e04663888fb
knarta <charquen10@gmail.com>
2025-04-23 14:03:00 +0200

    Add new module 0-basic_async_syntax: Implement async function to wait for a random delay with type annotations

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module 0-basic_async_syntax - Basic async syntax
+"""
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Wait for a random delay between 0 and max_delay seconds
+    Args:
+        max_delay: int = 10
+    Returns:
+        float: The random delay
+    """
+    return random.uniform(0, max_delay)
