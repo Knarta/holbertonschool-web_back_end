04a6b456d13e428fbec3a077189d97c32f251fdd
knarta <charquen10@gmail.com>
2025-04-23 14:10:07 +0200

    Add blank line in 0-basic_async_syntax module: Improve code readability by adding a blank line before the async function definition

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,7 @@ Module 0-basic_async_syntax - Basic async syntax
 import random
 import asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Wait for a random delay between 0 and max_delay seconds
