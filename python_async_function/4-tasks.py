8812a8375c2c8debfb5d97028d4f8458a40fa9a3
knarta <charquen10@gmail.com>
2025-04-23 15:03:40 +0200

    Change file mode of 4-tasks.py to executable and add a blank line for improved readability

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Wait for n random delays between 0 and max_delay seconds
