2bf1d602538bec7c0ab75caec99121ae60ab494e
knarta <charquen10@gmail.com>
2025-04-29 17:05:53 +0200

    Refactor get_hyper_index method in Server class: Update index validation, streamline data retrieval logic, and improve next index calculation for enhanced pagination accuracy and efficiency.

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -45,23 +45,25 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a dictionary with pagination information"""
-        assert isinstance(index, int) and 0 <= index < len(self.dataset())
+        assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
-        csv_data = self.dataset()
         data = []
         current_index = index
-        
-        for i in range(page_size):
-            while not csv.get(next):
-                next += 1
-            data.append(csv.get(next))
-            next += 1
-        
+        indexed_data = self.indexed_dataset()
+
+
+        while len(data) < page_size and current_index < len(indexed_data):
+            if current_index in indexed_data:
+                data.append(indexed_data[current_index])
+            current_index += 1
+
+        next_index = current_index if current_index < len(indexed_data) else None
+
         return {
             'index': index,
             'data': data,
-            'page_size': len(data),
-            'next_index' : next_index
+            'page_size': page_size,
+            'next_index': next_index
         }
         
