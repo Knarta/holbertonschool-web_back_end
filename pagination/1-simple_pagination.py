e6cf1886cc94a3079995f12131acdf0c1d4a6550
knarta <charquen10@gmail.com>
2025-04-29 11:05:26 +0200

    Refactor get_page method in Server class: Adjust indentation for improved readability and maintainability

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,13 +36,12 @@ class Server:
                 self.__dataset = dataset[1:]
         return self.__dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    assert isinstance(page, int) and isinstance(page_size, int)
-    assert page > 0 and page_size > 0
-    start, end = index_range(page, page_size)
-    dataset = self.dataset()
-
-    if start >= len(dataset):
-        return []
-    return dataset[start:end]
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
