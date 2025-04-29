ef4a3d02b553e5c319deba8e092fcd8e6ec1a33d
knarta <charquen10@gmail.com>
2025-04-29 10:28:11 +0200

    Add new module 0-simple_helper_function: Implement index_range function to calculate start and end indexes for pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module 0-simple_helper_function - Simple Helper Function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes
+    to return in a list for those particular
+    pagination parameters.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
