24a3b7fa94a7e9a48704da68f2854cb3d20dd570
knarta <charquen10@gmail.com>
2025-04-29 10:57:42 +0200

    Refactor dataset method in Server class: Adjust indentation for improved readability and maintainability

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -29,13 +29,13 @@ class Server:
         self.__dataset = None
 
 
-def dataset(self) -> List[List]:
-    if self.__dataset is None:
-        with open(self.DATA_FILE) as f:
-            reader = csv.reader(f)
-            dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-    return self.__dataset
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+                self.__dataset = dataset[1:]
+        return self.__dataset
 
 
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
