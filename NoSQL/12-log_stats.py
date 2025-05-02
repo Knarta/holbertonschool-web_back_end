48c3a2f5f4a49ef8e62145fc55f1cacbddb1b004
knarta <charquen10@gmail.com>
2025-05-02 21:37:19 +0200

    Add log statistics function for NoSQL: Create 12-log_stats.py to retrieve and display log statistics from the MongoDB nginx collection.

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Log stats
+"""
+from pymongo import MongoClient
+
+def log_stats():
+    """
+        Log stats
+    """
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    collection = db.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {}".format(collection.count_documents({"method": "GET"})))
+    print("\tmethod POST: {}".format(collection.count_documents({"method": "POST"})))
+    print("\tmethod PUT: {}".format(collection.count_documents({"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(collection.count_documents({"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(collection.count_documents({"method": "DELETE"})))
+    print("{} status check".format(collection.count_documents({"method": "GET", "path": "/status"})))
+
+if __name__ == "__main__":
+    log_stats()
