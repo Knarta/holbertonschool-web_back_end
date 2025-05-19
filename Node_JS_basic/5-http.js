aabe2429e8f7a073f9d44db2a7e385a966de8039
knarta <charquen10@gmail.com>
2025-05-19 19:25:59 +0200

    Add HTTP server with student listing: Create 5-http.js to implement an HTTP server that responds with a greeting and serves a list of students from a database, handling errors appropriately.

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,45 @@
+/*eslint-disable*/
+const http = require('http');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    
+    if (!DATABASE) {
+      res.statusCode = 500;
+      res.end('Cannot load the database');
+      return;
+    }
+
+    countStudents(DATABASE)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app; 
