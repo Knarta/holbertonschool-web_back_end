76f9e0812715359d6c179f3b8b4c3a686520dd1f
knarta <charquen10@gmail.com>
2025-05-20 15:41:59 +0200

    Refactor HTTP server response handling: Update 5-http.js to improve response structure by ensuring proper status codes and messages, and streamline the handling of student data retrieval.

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -10,34 +10,28 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   const { url } = req;
 
   if (url === '/') {
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
-    
-    if (!DATABASE) {
-      res.statusCode = 500;
-      res.end('Cannot load the database');
-      return;
-    }
-
     try {
-      await countStudents(DATABASE);
-      res.end();
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
     } catch (error) {
-      res.statusCode = 500;
       res.end(error.message);
     }
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(port, hostname);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
