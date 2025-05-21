44ed4e69b17dc07b331af40ef1d27fd8c9e6e4e0
knarta <charquen10@gmail.com>
2025-05-21 12:23:23 +0200

    Enhance server.js: Add JSON parsing middleware and improve server startup logging with a confirmation message for better visibility.

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,11 +1,15 @@
+/* eslint-disable */
 import express from 'express';
-import router from './routes';
+import router from './routes/index';
 
 const app = express();
 const port = 1245;
 
+app.use(express.json());
 app.use('/', router);
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
 
-export default app; 
+export default app;
