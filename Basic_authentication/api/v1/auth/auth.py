ec485e8f729c686d10933f837e49911bd129206d
Knarta <charlene.martin@holbertonstudents.com>
2025-10-27 18:07:53 +0100

    Refactor require_auth method: Remove unnecessary whitespace for improved code readability and maintainability.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,15 +14,15 @@ class Auth:
         """
         if path is None:
             return True
-            
+
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         path_slash = path if path.endswith('/') else path + '/'
-        
+
         if path_slash in excluded_paths:
             return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
