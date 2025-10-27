25614c393d690e3919d0a4931ab6ada9e4840ee9
Knarta <charlene.martin@holbertonstudents.com>
2025-10-27 18:05:25 +0100

    Enhance require_auth method logic: Update the require_auth method to handle None and empty excluded_paths cases, improving authentication path checks.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth
         """
-        return False
+        if path is None:
+            return True
+            
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        path_slash = path if path.endswith('/') else path + '/'
+        
+        if path_slash in excluded_paths:
+            return False
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header
