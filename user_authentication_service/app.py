ff453b96443d2ed6fa7058036ff964dbc78e817a
Knarta <charlene.martin@holbertonstudents.com>
2025-11-19 15:51:09 +0100

    Add logout endpoint to Flask app: Implement a new route for user logout that terminates the user session and redirects to the home page, enhancing session management and user experience in the application.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app module
 """
-from flask import Flask, abort, jsonify, make_response, request
+from flask import Flask, abort, jsonify, make_response, redirect, request
 
 from auth import Auth
 
@@ -45,6 +45,20 @@ def login():
     response = make_response(jsonify({"email": email, "message": "logged in"}))
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """Logout endpoint
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    
+    if user is None:
+        abort(403)
+    
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
