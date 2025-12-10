eaad4117657bbec4ee23e6573d64de356f796e9b
knarta <charquen10@gmail.com>
2025-12-10 14:29:20 +0100

    Refactor unit tests for sendPaymentRequestToApi: Update test structure for clarity, using assert for verification and improving readability of stubbing and spying on Utils.calculateNumber and console.log.

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,29 +1,22 @@
-const sinon = require('sinon');
-const { expect } = require('chai');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./4-payment");
+const Utils = require("./utils");
+const assert = require("assert");
 
-describe('sendPaymentRequestToApi', function() {
-    it('should stub Utils.calculateNumber to return 10 and verify console.log', function() {
-      // Create a stub on Utils.calculateNumber that always returns 10
-      const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-      
-      // Create a spy on console.log
-      const consoleSpy = sinon.spy(console, 'log');
-  
-      // Call the function we're testing
-      sendPaymentRequestToApi(100, 20);
-  
-      // Verify the stub was called with the correct arguments
-      expect(stub.calledOnce).to.be.true;
-      expect(stub.calledWith('SUM', 100, 20)).to.be.true;
-  
-      // Verify console.log was called with the correct message
-      expect(consoleSpy.calledOnce).to.be.true;
-      expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
-  
-      // Restore the stub and spy
-      stub.restore();
-      consoleSpy.restore();
-    });
+describe("Send payment Request to Api", function () {
+  it("should stub Utils.calculateNumber and spy on console.log", function () {
+    const stub = sinon.stub(Utils, "calculateNumber").returns(10);
+
+    const spy = sinon.spy(console, "log");
+
+    sendPaymentRequestToApi(100, 20);
+
+    assert(stub.calledOnceWithExactly("SUM", 100, 20));
+
+    assert(spy.calledOnceWithExactly("The total is: 10"));
+
+    stub.restore();
+
+    spy.restore();
   });
+});
