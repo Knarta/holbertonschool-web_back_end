4656708004538108038e952b2d0586de74052c87
knarta <charquen10@gmail.com>
2025-12-10 14:06:01 +0100

    Refactor calculateNumber function: Convert Utils object method to standalone function, improve error handling with switch statement, and update module export format for better clarity and usability.

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -1,19 +1,17 @@
-const Utils = {
-    calculateNumber(type, a, b) {
-      const roundedA = Math.round(a);
-      const roundedB = Math.round(b);
+function calculateNumber(type, a, b) {
+    const x = Math.round(a);
+    const y = Math.round(b);
   
-      if (type === 'SUM') {
-        return roundedA + roundedB;
-      } else if (type === 'SUBTRACT') {
-        return roundedA - roundedB;
-      } else if (type === 'DIVIDE') {
-        if (roundedB === 0) {
-          return 'Error';
-        }
-        return roundedA / roundedB;
-      }
+    switch (type) {
+      case "SUM":
+        return x + y;
+      case "SUBTRACT":
+        return x - y;
+      case "DIVIDE":
+        return y === 0 ? "Error" : x / y;
+      default:
+        throw new Error("Calcul not found.");
     }
-  };
+  }
   
-  module.exports = Utils;
+  module.exports = { calculateNumber };
